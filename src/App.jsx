bdbaa19174ff3291d0a3208dedbe6379bb61858b
nalsae <nalsae0207@naver.com>
2023-07-11 15:53:31 +0900

    [FEAT] App 컴포넌트에 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main';
+import ProductsList from 'pages/ProductsList';
+import BookMark from 'pages/Bookmark';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/products/list" element={<ProductsList />}></Route>
+      <Route path="/bookmark" element={<BookMark />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
