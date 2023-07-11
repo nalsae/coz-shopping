643734b2abf93ce768d53594c0bc955a507a041b
nalsae <nalsae0207@naver.com>
2023-07-11 17:21:55 +0900

    [FEAT] App 컴포넌트에 Header 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
+import Header from 'components/Header';
 import Main from 'pages/Main';
 import ProductsList from 'pages/ProductsList';
 import BookMark from 'pages/Bookmark';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/products/list" element={<ProductsList />}></Route>
-      <Route path="/bookmark" element={<BookMark />}></Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/products/list" element={<ProductsList />}></Route>
+        <Route path="/bookmark" element={<BookMark />}></Route>
+      </Routes>
+    </>
   );
 }
 
