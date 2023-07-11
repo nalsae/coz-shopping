40874038f2dd09344580f294bec1b16226417a8f
nalsae <nalsae0207@naver.com>
2023-07-11 15:51:06 +0900

    [FEAT] index.js에 라우팅, normalize CSS 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { NormalizeStyle } from 'styles/normalizeStyle';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <Router>
+      <NormalizeStyle />
+      <App />
+    </Router>
   </StrictMode>
 );
