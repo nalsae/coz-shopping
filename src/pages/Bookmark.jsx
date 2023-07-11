f3412ecdaf1abbf58ffaa87d6fcd9204fe602c93
nalsae <nalsae0207@naver.com>
2023-07-11 15:53:09 +0900

    [FEAT] 임시 라우팅 구현을 위해 임시 페이지 컴포넌트 구현

---
+++ src/pages/Bookmark.jsx
@@ -0,0 +1,5 @@
+function BookMark() {
+  return <main>나는 북마크 페이지!</main>;
+}
+
+export default BookMark;
---
+++ src/pages/ProductsList.jsx
@@ -0,0 +1,5 @@
+function ProductsList() {
+  return <main>나는 상품 리스트 페이지!</main>;
+}
+
+export default ProductsList;
