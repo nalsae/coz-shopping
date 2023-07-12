2a6eda37a353038c01b281399fa59f81ddc34074
nalsae <nalsae0207@naver.com>
2023-07-13 02:03:37 +0900

    [FEAT] Toast 컴포넌트 상태 관리를 위해 관련 action 객체 추가

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,6 +2,8 @@ export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 export const FETCH_PRODUCTS_ERROR = 'FETCH_PRODUCTS_ERROR';
 export const ADD_BOOKMARK = 'ADD_BOOKMARK';
 export const DELETE_BOOKMARK = 'DELETE_BOOKMARK';
+export const PUSH_TOAST = 'PUSH_TOAST';
+export const SHIFT_TOAST = 'SHIFT_TOAST';
 
 export const fetchProductsSuccess = (products) => ({
   type: FETCH_PRODUCTS_SUCCESS,
@@ -25,4 +27,13 @@ export const deleteBookmark = (id) => ({
   payload: {
     id,
   },
+});
+
+export const pushToast = (isBookmarked) => ({
+  type: PUSH_TOAST,
+  payload: isBookmarked,
+});
+
+export const shiftToast = () => ({
+  type: SHIFT_TOAST,
 });
