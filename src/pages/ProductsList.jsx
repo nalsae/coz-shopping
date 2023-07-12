0759f4c9c53c13329829ee0108dcf2cf94b66826
nalsae <nalsae0207@naver.com>
2023-07-12 15:30:09 +0900

    [FEAT] ProductsList 페이지 컴포넌트 구현

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Products from 'components/Products';
+import Filters from 'components/Filters';
+import setInitialFilter from 'utils/functions/setInitialFilter';
+import filterProducts from 'utils/functions/filterProducts';
+
 function ProductsList() {
-  return <main>나는 상품 리스트 페이지!</main>;
+  const { products } = useSelector((state) => state.productsReducer);
+  const [currentFilter, setCurrentFilter] = useState(
+    setInitialFilter('productsFilter')
+  );
+
+  const filtered = filterProducts(products, currentFilter, false);
+
+  return (
+    <main>
+      <h2 className="srOnly">상품 리스트 페이지</h2>
+      <Filters
+        currentFilter={currentFilter}
+        setCurrentFilter={setCurrentFilter}
+        filterType="products"
+      />
+      <Products productsInfo={filtered} />
+    </main>
+  );
 }
 
 export default ProductsList;
