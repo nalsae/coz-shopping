ebabf1091c050af4524e84ea35898c90d8d4e755
nalsae <nalsae0207@naver.com>
2023-07-14 01:31:51 +0900

    [REFACTOR] 생성한 유틸 상수 적용

--- src/actions/fetchProducts.js
+++ src/actions/fetchProducts.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { fetchProductsSuccess, fetchProductsError } from './actions';
+import { STORAGE_KEYS } from 'utils/constants';
 
 export const fetchProducts = () => async (dispatch) => {
   try {
-    const storageData = localStorage.getItem('persist:cozShopping');
+    const storageData = localStorage.getItem(STORAGE_KEYS.cozShopping);
 
     if (storageData) {
       dispatch(fetchProductsSuccess(storageData));
