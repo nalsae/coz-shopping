c6c8338bda65034aba689551e8cca7cc28eaf258
nalsae <nalsae0207@naver.com>
2023-07-14 01:54:05 +0900

    [FEAT] 재사용을 위해 mixin 생성

---
+++ src/styles/mixin.js
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+const LINK_STYLE = css`
+  display: flex;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  padding-left: 54px;
+  background-position: 26px 15px;
+`;
+
+const SELECTED_FILTER_STYLE = css`
+  font-weight: 800;
+  color: #412dd4;
+  border-bottom: 3px solid #412dd4;
+`;
+
+export { LINK_STYLE, SELECTED_FILTER_STYLE };
