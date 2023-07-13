579bc65b03a12241f423123f400f5be55d3b9b2e
nalsae <nalsae0207@naver.com>
2023-07-14 01:30:56 +0900

    [REFACTOR] 상수화를 위한 유틸 객체 생성

--- src/utils/constants/filterInfo.js
+++ src/utils/constants/filterInfo.js
@@ -1,15 +1,15 @@
-import all from 'assets/all.png';
-import products from 'assets/products.png';
-import category from 'assets/category.png';
-import exhibition from 'assets/exhibition.png';
-import brand from 'assets/brand.png';
+import all from 'assets/imgs/all.png';
+import products from 'assets/imgs/products.png';
+import category from 'assets/imgs/category.png';
+import exhibition from 'assets/imgs/exhibition.png';
+import brand from 'assets/imgs/brand.png';
 
-const FILTER_INFO = [
+const FILTER_INFO = Object.freeze([
   { type: 'All', name: '전체', imgUrl: all },
   { type: 'Product', name: '상품', imgUrl: products },
   { type: 'Category', name: '카테고리', imgUrl: category },
   { type: 'Exhibition', name: '기획전', imgUrl: exhibition },
   { type: 'Brand', name: '브랜드', imgUrl: brand },
-];
+]);
 
 export default FILTER_INFO;
