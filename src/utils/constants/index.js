db3f5c2db1d4fa01f6f1cbe89561c2e36345f2ff
nalsae <nalsae0207@naver.com>
2023-07-17 01:47:44 +0900

    [FEAT] 상수화를 위한 유틸 객체 생성

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,5 +2,14 @@ import FILTER_INFO from './filterInfo';
 import FILTER_TYPE from './filterType';
 import STORAGE_KEYS from './storageKeys';
 import PRODUCT_TYPES from './productTypes';
+import TOAST_CONTENTS from './toastContents';
+import ARIA_LABELS from './ariaLabels';
 
-export { FILTER_INFO, FILTER_TYPE, STORAGE_KEYS, PRODUCT_TYPES };
+export {
+  FILTER_INFO,
+  FILTER_TYPE,
+  STORAGE_KEYS,
+  PRODUCT_TYPES,
+  TOAST_CONTENTS,
+  ARIA_LABELS,
+};
---
+++ src/utils/constants/toastContents.js
@@ -0,0 +1,6 @@
+const TOAST_CONTENTS = {
+  delete: '상품이 북마크에서 제거되었습니다.',
+  add: '상품이 북마크에 추가되었습니다.',
+};
+
+export default TOAST_CONTENTS;
