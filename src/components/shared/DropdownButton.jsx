55bb634f19bb80140673cc4e0d18f488522c57cd
nalsae <nalsae0207@naver.com>
2023-07-17 01:48:17 +0900

    [REFACTOR] DropdownButton 컴포넌트의 aria-label 텍스트 상수화

--- src/components/shared/DropdownButton.jsx
+++ src/components/shared/DropdownButton.jsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import { ReactComponent as Hamburger } from 'assets/icons/hamburger.svg';
+import { ARIA_LABELS } from 'utils/constants';
 
 function DropdownButton({ isOpen, setIsOpen }) {
   const handleClick = () => setIsOpen((prevValue) => !prevValue);
 
+  const { hamburgerButton } = ARIA_LABELS;
+
   return (
     <HamburgerButton
       type="button"
-      aria-label={isOpen ? '메뉴 닫기' : '메뉴 열기'}
+      aria-label={isOpen ? hamburgerButton.close : hamburgerButton.open}
       onClick={handleClick}
     >
       <Hamburger />
